16544c4190e5c609776067d9cea460adc768331b
Atia Rasida Silvia <atiasilvia0@gmail.com>
2023-12-31 23:02:01 +0600

    FoodCategory key added

--- Planorama/DAL/Models/FoodCategory.cs
+++ Planorama/DAL/Models/FoodCategory.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,15 @@ namespace DAL.Models
 {
     public class FoodCategory
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
 
+        [Required]
+        [StringLength(200)]
         public string Description { get; set; }
     }
 }
