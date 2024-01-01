56476f53c77a0b073d4cdaa71f3ce2e75ae2868e
Atia Rasida Silvia <atiasilvia0@gmail.com>
2024-01-01 12:42:11 +0600

    ems context

---
+++ Planorama/DAL/Migrations/Configuration.cs
@@ -0,0 +1,36 @@
+﻿namespace DAL.Migrations
+{
+    using DAL.Models;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<DAL.Models.EMSContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(DAL.Models.EMSContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+            for (int i = 1; i <= 6; i++)
+            {
+                context.FoodsCategory.AddOrUpdate(new Models.FoodsCategory
+                {
+                    Id = i,
+                    Name = Guid.NewGuid().ToString().Substring(0, 15),
+                    UName = "User-" + i,
+                    Password = Guid.NewGuid().ToString().Substring(0, 10),
+                    Type = "General"
+
+                });
+            }
+        }
+    }
+}
---
+++ Planorama/DAL/Models/EMSContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    internal class EMSContext : DbContext
+    {
+        public DbSet<FoodsCategory> FoodsCategories { get; set;}
+        public DbSet<Food> Foods { get; set;}
+        public DbSet<DecorationsCategory> DecorationsCategories {  get; set;}
+        public DbSet<Decoration> Decorations { get; set; }
+        public DbSet<LocationsCategory> LocationsCategories { get; set; }
+        public DbSet<Location> Locations { get; set; }
+
+
+
+
+
+
+    }
+}
