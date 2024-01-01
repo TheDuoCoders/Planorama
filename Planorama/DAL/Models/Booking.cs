8fb6a63c2efb12519a3607fbe0ad3e155e5a495d
Mohammad Abdur Rouf Hrithik <rroufislam@yahoo.com>
2024-01-01 12:54:18 +0600

    user, booking, coupon, review, notif added

---
+++ Planorama/DAL/Models/Booking.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Booking
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int PackageId { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        [ForeignKey("User")]
+        public string OrderedBy { get; set; }
+
+        [Required]
+        public DateTime OrderTime { get; set; }
+
+        public virtual User User { get; set; }
+    }
+}
