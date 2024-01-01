00f2404d63f3c7564901f2d34ad727c3e00e1840
Atia Rasida Silvia <atiasilvia0@gmail.com>
2024-01-01 20:02:03 +0600

    6th services done

---
+++ Planorama/BLL/Services/DecorationServices.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using DAL;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class DecorationServices
+    {
+        public static List<DecorationDTO> Get()
+        {
+            var data = DataAccessFactory.DecorationData().Read();
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Decoration, DecorationDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<List<DecorationDTO>>(data);
+            return mapped;
+        }
+
+        public static DecorationDTO Get(int id)
+        {
+            var data = DataAccessFactory.DecorationData().Read(id);
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<Decoration, DecorationDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<DecorationDTO>(data);
+            return mapped;
+
+        }
+    }
+}
---
+++ Planorama/BLL/Services/DecorationsCategoryServices.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using BLL.DTO;
+using DAL;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Services
+{
+    public class DecorationsCategoryServices
+    {
+        public static List<DecorationsCategoryDTO> Get()
+        {
+            var data = DataAccessFactory.DecorationsCategoryData().Read();
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<DecorationsCategory, DecorationsCategoryDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<List<DecorationsCategoryDTO>>(data);
+            return mapped;
+        }
+
+        public static DecorationsCategoryDTO Get(int id)
+        {
+            var data = DataAccessFactory.DecorationsCategoryData().Read(id);
+            var cfg = new MapperConfiguration(c =>
+            {
+                c.CreateMap<DecorationsCategory, DecorationsCategoryDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var mapped = mapper.Map<DecorationsCategoryDTO>(data);
+            return mapped;
+
+        }
+    }
+}
