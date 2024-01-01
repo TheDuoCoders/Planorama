8a058ebdf0efea4d8236a939faf013beadf363d6
Mohammad Abdur Rouf Hrithik <rroufislam@yahoo.com>
2024-01-01 13:28:24 +0600

    notifcation updated

--- Planorama/DAL/Models/Notification.cs
+++ Planorama/DAL/Models/Notification.cs
@@ -5,6 +5,8 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.Remoting.Contexts;
 
 namespace DAL.Models
 {
